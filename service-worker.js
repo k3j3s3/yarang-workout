38b2f080364d04dc19ef8572abff40513081eebc
k3j3s3 <k3j3s3@gmail.com>
2026-03-26 15:22:31 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "yarang-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
